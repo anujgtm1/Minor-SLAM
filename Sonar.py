06cf78f2216fc1d2b8279c8393e97f406863ef91
Anuj Gautam <anuj_gtm1@hotmail.com>
2015-08-01 14:50:25 +0000

    The code for distance calculation of sonar module has been drafted. A file blinker.py was added to test the GPIO pins and was deemed successful. The file is temporary and will be removed later.

--- Sonar.py
+++ Sonar.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 import RPi.GPIO as io
+import time
 
 io.setmode(io.BOARD)
 io.setwarnings(false)
 
+ECHO=8
+TRIG=10
+SoundSpeed = 33000
+
+io.setup(ECHO, io.in)
+io.setup(TRIG, io.out)
+
+io.output(TRIG, io.LOW)
+time.sleep(0.0000005)
+io.output(TRIG, io.HIGH)
+time.sleep(0.000001)
+
+if io.input(ECHO)==true:
+	start = time.time()
+
+if io.input(ECHO)==false:
+	stop = time.time()
+
+total=stop-start
+
+distance = total*SoundSpeed
+
+print distance
+
+io.cleanup()
 
