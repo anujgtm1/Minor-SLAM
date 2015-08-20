9d1cf0a47dfe9f7371817615e4f24a19ce0c9ac8
codeezer <code_ezer@live.com>
2015-08-20 19:57:16 +0545

    la hai ta :P

--- pyHwModules/toCart.py
+++ pyHwModules/toCart.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 def toCartesian(x):
-	y = x[:,1]*np.sin(x[:,0])
-	x[:,0] = x[:,1]*np.cos(x[:,0])
+	y = x[:,1]*np.sin(np.deg2rad(x[:,0]))
+	x[:,0] = x[:,1]*np.cos(np.deg2rad(x[:,0]))
 	x[:,1] = y
 	return x
