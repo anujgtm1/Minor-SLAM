1bf15260426ee796f8db8f9eda62332be07f2512
Anuj Gautam <anuj_gtm1@hotmail.com>
2015-08-24 16:44:42 +0000

    Bot methods added and code further corrected.

--- Bot.py
+++ Bot.py
@@ -1,33 +1,76 @@
 class Bot():
 	import numpy as np
 	import plot as pt
+	import interact
+	import toCart
 	
 	
 	def __init__(self):
 		self.Map = np.empty(shape=(0,2), dtype=np.float32)
+		#Position : x, y, theta
 		self.Pos = np.array([[0,0,0]], dtype=np.float32)
 		self.Batch = np.empty(shape=(0,2), stype=np.float32)
+		a =  #measured practical distance travelled upon single pulse forward movement
+		return
 		
 	def plot(self):
 		#plot the map
 		pt.plotArray(self.Map)
+		return
 		
-	def survey(self):
-		#send getData singal to the arduino and get the data back
+	def observe(self):
+		#send getData signal to the arduino and get the data back
 		#get data into self.Batch
+		#Cartesian conversion from Polar coordinates.
+		self.Batch = interact.observe()
+		toCart.toCartesian(self.Batch)
+		return
 	
-	def append(self):
+	def transform(self):
 		#manipulate the self.Batch array using the self.Pos array to use it in absolute map
-		x = self.Batch[:,0] * np.cos(np.deg2rad(self.Pos[0,2])) - self.Batch[:,1] * np.sin(np.deg2rad(self.Pos[0,2]))
-		self.Batch[:,1] = self.Batch[:,0] * np.sin(np.deg2rad(self.Pos[0,2])) + self.Batch[:,1] * np.cos(np.deg2rad(self.Pos[0,2]))
+		#first rotation in xy plane and then translation to the required point(current position)
+		sinTheta = np.sin(np.deg2rad(self.Pos[0,2]))
+		cosTheta = np.cos(np.deg2rad(self.Pos[0,2]))
+		x = self.Batch[:,0] * cosTheta - self.Batch[:,1] * sinTheta
+		self.Batch[:,1] = self.Batch[:,0] * sinTheta + self.Batch[:,1] * cosTheta
 
 		self.Batch[:,0] = x + self.Pos[0,0]
 		self.Batch[:,1] = self.Batch[:,1] + self.Pos[0,1]
-
-		#append it to the self.Map
-		self.Map = np.append(self.Map, self.Batch, axis=0)
+		return
 		
+	def append(self):
+		#append self.Batch to the self.Map
+		self.Map = np.append(self.Map, self.Batch, axis=0)
+		return
+	
+	def fwd(self):
+		interact.mov_forward()
+		self.Pos[0,0] += self.a*np.cos(np.deg2rad(self.Pos[0,2]))
+		self.Pos[0,1] -= self.a*np.sin(np.deg2rad(self.Pos[0,2]))
+		return
+		
+	def back(self):
+		interact.mov_back()
+		self.Pos[0,0] -= self.a*np.cos(np.deg2rad(self.Pos[0,2]))
+		self.Pos[0,1] += self.a*np.sin(np.deg2rad(self.Pos[0,2]))
+		return
+	
+	def left(self):
+		interact.mov_left()
+		#Does moving left have any effect on position?
+		
+		self.Pos[0,2] += 90
+		return
+		
+	def right(self):
+		interact.mov_right()
+		#Does moving right have any effect on position?
+		
+		self.Pos[0,2] -= 90
+		return
+	
 	def reset(self):
 		self.Map = np.empty(shape=(0,2), dtype=np.float32)
 		self.Pos = np.array([[0,0,0]], dtype=np.float32)
 		self.Batch = np.empty(shape=(0,2), stype=np.float32)
+		return
